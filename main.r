2f077ed88544746bbb4691ba00aa53056bb134d9
Mathieu Bouville <mathieu.bouville@gmail.com>
2015-02-17 09:40:54 +0100

    Updated data (as of 28th February 2015). Stcok allocation: 84%

--- main.r
+++ main.r
@@ -162,6 +162,8 @@ start <- function(dataSplit="all",           # "all" for all data, "training" an
    createTypicalStrategies(force=force)
    
    showSummaries()
+   print(paste0("RECOMMENDED STOCK ALLOCATION: ", 
+                round( 100*stats$latestStockAlloc[which(stats$strategy==typical$balanced)]), "%"))
 
    if(otherAssetClasses) 
       if (dat$numericDate[numData] < 1968) # if there are no data after 1968 in 'dat' (training mode) 
@@ -220,7 +222,8 @@ startTesting <- function(futureYears=10L, tradingCost=0.5/100, riskAsCost=1.5/10
    if (!otherAssetClasses)
       plotAllReturnsVsFour()
 }
-# startTesting(lastMonthSP500=2054.05)       # average of S&P500 through 30th December 2014
+# startTesting(lastMonthSP500=2081.0)    # average of S&P500 through 26th Feb. 2015 (source: stlouisfed.org)
+# startTesting(lastMonthSP500=2081.0, downloadAndCheckAllFiles=T, otherAssetClasses=T)
 
 
 # plotAllReturnsVsFour()
